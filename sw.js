c64f5c1210dbc010bb4634342aa8ef3d32e474f1
GabixBoxing <contacto.osvimer@gmail.com>
2026-05-24 14:58:44 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+var CACHE_NAME = 'cba-boxing-v3';
+var urlsToCache = ['/'];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(keys.filter(function(k){ return k !== CACHE_NAME; }).map(function(k){ return caches.delete(k); }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(r) {
+      return r || fetch(e.request).then(function(res) {
+        var clone = res.clone();
+        caches.open(CACHE_NAME).then(function(c){ c.put(e.request, clone); });
+        return res;
+      }).catch(function(){ return caches.match('/'); });
+    })
+  );
+});
